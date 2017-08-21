9216497b65edb3674233269d1ea56410dd7d43af
shiva <shiva@cc.com>
2017-08-21 15:01:55 +0530

    Java Proh\gram File

---
+++ Puppy.java
@@ -0,0 +1,11 @@
+public class Puppy
+{
+public Puppy(String name)
+{
+System.out.println("Passed name is :" + name);
+}
+public static void main(String arg[])
+{
+Puppy Newpuppy = new Puppy("Tommy");
+}
+}
